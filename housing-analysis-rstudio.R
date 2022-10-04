33fbe9b7d76e1c089b8164f4f9fe0c839172a37a
Rachael H <85131921+rachh8283@users.noreply.github.com>
2022-10-04 13:02:23 -0600

    Update housing-analysis-rstudio.R

--- housing-analysis-rstudio.R
+++ housing-analysis-rstudio.R
@@ -15,13 +15,16 @@ library(broom)
 library(ggpubr)
 
 # Read training and testing data sets into RStudio
-setwd("X:\RACHAEL\Freelance\Samples\SAS + R")
+setwd("D:/R")
 housesTrain<- read_csv("training.csv")
 housesTest <- read_csv("testing.csv")
 
+Sys.time()
 # Summary of the Sale Price in  the test data set
 summary(housesTest$SalePrice)
+summary(housesTrain$SalePrice)
 
+Sys.time()
 # Plot histogram of sales price for test data set
 options(scipen=999)
 hist(housesTest$SalePrice, labels=TRUE, main="Distribution of Sale Price in Testing Data",
@@ -30,6 +33,7 @@ par(new=TRUE)
 boxplot(housesTest$SalePrice, horizontal = TRUE, axes=FALSE,col="yellow")
 box()
 
+Sys.time()
 # Combine training and testing files
 Combo <- combine(housesTest, housesTrain)
 
@@ -40,10 +44,12 @@ par(new=TRUE)
 boxplot(Combo$SalePrice, horizontal = TRUE, axes=FALSE,col="yellow")
 box()
 
+Sys.time()
 # Linear regression model on training set
 fit <- lm(SalePrice ~., data=housesTrain)
 summary(fit)
 
+Sys.time()
 # Remove all the rows with missing values in Testing data set
 housesTest <- na.omit(housesTest)
 # Read only the first 20 rows from test data to predict
@@ -52,6 +58,7 @@ test20 <- housesTest[1:20,]
 predict_price <- predict(fit, newdata=test20)
 tidy(predict_price)
 
+Sys.time()
 # Compare predicted to actual prices
 df1 <- data.frame(x=1:20, actual=test20$SalePrice, predicted=predict_price)
 df1
@@ -61,4 +68,7 @@ lines(df1$x, df1$predicted, type = "b", pch=18, col = "blue", lty=1)
 legend("topright", legend=c("Actual Prices", "Predicted Prices"), col=c("red", "blue"), 
        lty=1, cex=0.8)
 
-#rm(list=ls())
+
+# Clear Environment and Graphics
+rm(list=ls())
+graphics.off()
